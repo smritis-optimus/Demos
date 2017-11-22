9561da81eb89c4918b74ed6bcc7e31df70793462
Smriti Singh <smriti.singh@optimusinfo.com>
2017-11-23 00:53:43 +0530

    Added demo files

---
+++ AngularDemos/AngularDemo1/Scripts/Script.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+//creating a module
+//parameters : name of module, passing array
+var myApp = angular.module("myModule", []);
+
+//create a function and assign to variable
+var myController = function($scope) {
+    $scope.message = "Angular JS Demo";
+}
+//set the controller to module
+//parameters : name of controller, controller
+myApp.controller("myController", myController);
+//or 
+myApp.controller("myController", function($scope) {
+    $scope.message = "Angular JS Demo";
+});
